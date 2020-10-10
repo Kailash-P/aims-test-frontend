3803859f4b50327631d91f7fbdfcfcecdd2fbc56
Kailash-P <mr.p.kailash@gmail.com>
2020-10-10 12:30:08 +0530

    ResetPassword, PasswordResetSuccess page changes

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ForgotPassword from "./app/components/ForgotPassword/views/ForgotPassword";
+import PasswordResetSuccess from "./app/components/ForgotPassword/views/PasswordResetSuccess";
+import ResetPassword from "./app/components/ForgotPassword/views/ResetPassword";
 import VerificationCodeSent from "./app/components/ForgotPassword/views/VerificationCodeSent";
 import LandingPage from "./app/components/LandingPage/views/LandingPage";
 import SignUp from "./app/components/SignUp/views/SingUp";
@@ -14,6 +16,8 @@ function Routes() {
         <Route exact path="/signUp" component={SignUp} />
         <Route exact path="/forgotPassword" component={ForgotPassword} />
         <Route exact path="/verificationCodeSent" component={VerificationCodeSent} />
+        <Route exact path="/resetPassword" component={ResetPassword} />
+        <Route exact path="/passwordResetSuccess" component={PasswordResetSuccess} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/app/components/ForgotPassword/views/PasswordResetSuccess.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(30),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  subtitle1: {
+      fontSize: 13,
+    }
+}));
+
+export default function PasswordResetSuccess() {
+    const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <CheckCircleIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Password Reset Successfull
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Typography className={classes.subtitle1} align='center'>
+                Please sign in to your account with new credentials
+          </Typography>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            href="/"
+          >
+            Ok
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+}
