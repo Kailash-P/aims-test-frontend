95c69db4017991362da77e335a7040dddbf78cc4
Kailash-P <mr.p.kailash@gmail.com>
2020-10-27 13:31:23 +0530

    Updates

---
+++ precache-manifest.07e0fb342a4939186f87c87fe66ef95d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714765a031fe3cdb18c0c068857b1f71",
+    "url": "/aims-test-frontend/index.html"
+  },
+  {
+    "revision": "504e1caab647b88b72ef",
+    "url": "/aims-test-frontend/static/js/2.747843ce.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/aims-test-frontend/static/js/2.747843ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382d848e4a0a3f34086c",
+    "url": "/aims-test-frontend/static/js/main.ac4c56aa.chunk.js"
+  },
+  {
+    "revision": "5221829e02cb13e21597",
+    "url": "/aims-test-frontend/static/js/runtime-main.17f9f9ec.js"
+  }
+]);
