b4503f16bc310ea49ea551b460205dfea89e0c3d
Kailash-P <mr.p.kailash@gmail.com>
2020-11-07 11:40:03 +0530

    Code changes to include dynamic menu list items.

--- src/app/components/Menu/views/MenuLandingPage.js
+++ src/app/components/Menu/views/MenuLandingPage.js
@@ -12,7 +12,6 @@ import IconButton from "@material-ui/core/IconButton";
 import Container from "@material-ui/core/Container";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import { mainListItems } from "./MenuListItems";
 import UserProfile from "../../Base/views/UserProfile";
 import AppsIcon from "@material-ui/icons/Apps";
 import Tooltip from "@material-ui/core/Tooltip";
@@ -112,7 +111,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function MenuLandingPage() {
+export default function MenuLandingPage({ menuListItems }) {
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
 
@@ -182,7 +181,7 @@ export default function MenuLandingPage() {
             </IconButton>
           </div>
           <Divider />
-          <List>{mainListItems}</List>
+          <List>{menuListItems}</List>
           <Divider />
         </Drawer>
         <main className={classes.content}>
--- src/app/components/base/views/base.js
+++ src/app/components/base/views/base.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import MenuLandingPage from '../../Menu/views/MenuLandingPage'
+import { mainListItems } from "../../Menu/views/MenuListItems";
 
 export default function base() {
     return (
         <>
-            <MenuLandingPage/>
+            <MenuLandingPage menuListItems={mainListItems}/>
         </>
     )
 }
